eb224e3ddadf4f3a42bc0143444c219f55aecc1a
Thomas Steinke <exyphnos@gmail.com>
2014-01-25 00:19:39 -0800

    FIXING THINGS AS USUAL

--- GGJam-desktop/src/com/band/ggjam/Main.java
+++ GGJam-desktop/src/com/band/ggjam/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "GGJam";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 768;
+		cfg.height = 512;
 		
 		new LwjglApplication(new GGJam(), cfg);
 	}
