2a2e41200d58e6fb71368b823d33776475cbfa28
Thomas Steinke <exyphnos@gmail.com>
2014-01-25 13:50:29 -0800

    Wat

--- GGJam/src/com/band/ggjam/Emitter.java
+++ GGJam/src/com/band/ggjam/Emitter.java
@@ -4,17 +4,26 @@ public class Emitter extends Entity {
 	public static int SHOT_TIMER = 4;
 	
 	int shotDelay;
+	private int frame;
 	
 	public Emitter(int x, int y, Level level) {
 		super(x, y, Art.entities[0][0], level);
+		
+		frame = 0;
 	}
 	
 	public void tick() {
+		frame += 2;
+		if(frame >= Laser.LASER_FRAMES) frame = 0;
 		if(shotDelay++ > SHOT_TIMER) {
-			currentLevel.add(new Laser(x, y, currentLevel, Laser.LASER_SPEED, 0), true);
-			currentLevel.add(new Laser(x, y, currentLevel, -Laser.LASER_SPEED, 0), true);
-			currentLevel.add(new Laser(x, y, currentLevel, 0, -Laser.LASER_SPEED), true);
-			currentLevel.add(new Laser(x, y, currentLevel, 0, Laser.LASER_SPEED), true);
+			currentLevel.add(new Laser(x, y, currentLevel, Laser.LASER_SPEED, 0, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, -Laser.LASER_SPEED, 0, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, 0, -Laser.LASER_SPEED, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, 0, Laser.LASER_SPEED, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, Laser.LASER_SPEED, Laser.LASER_SPEED, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, -Laser.LASER_SPEED, -Laser.LASER_SPEED, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, Laser.LASER_SPEED, -Laser.LASER_SPEED, frame), true);
+			currentLevel.add(new Laser(x, y, currentLevel, -Laser.LASER_SPEED, Laser.LASER_SPEED, frame), true);
 		}
 	}
 }
--- GGJam/src/com/band/ggjam/Laser.java
+++ GGJam/src/com/band/ggjam/Laser.java
@@ -3,13 +3,14 @@ package com.band.ggjam;
 
 public class Laser extends Entity {
 	public static final float LASER_SPEED = 4;
+	public static final int LASER_FRAMES = 10;
 
 	private int frame;
 	
 	private float dx, dy;
 	private boolean toBeDead = false;
 	
-	public Laser(float x, float y, Level level, float dx, float dy) {
+	public Laser(float x, float y, Level level, float dx, float dy, int frameStart) {
 		super(x, y + 10, Art.laser[0][0], level);
 		
 		this.dx = dx;
@@ -18,18 +19,36 @@ public class Laser extends Entity {
 		float angle = (float) (Math.atan(dy / dx) * 180 / Math.PI);
 		setOrigin(16,6);
 		setRotation(angle - 90);
+		
+		if(dx < 0)
+			setRotation(angle - 270);
+		
+		frame = frameStart;
 	}
 	
 	public void tick() {
+		setRegion(Art.laser[0][frame * 2 / LASER_FRAMES]);
+		if(++frame >= LASER_FRAMES) frame = 0;
+		
 		if(toBeDead)
 			dead = true;
 		
 		tryMove(dx, dy);
 	}
 	
+//	public float getWidth() {
+//		if(this.getRotation() % 180 != 0)
+//			return 1;
+//		else 
+//			return 1;
+//	}
+	
 	public void hitWall(float dx, float dy) {
 		toBeDead = true;
 		setRegion(Art.laser[0][2]);
+		
+		x += dx;
+		y +=dy;
 	}
 	
 	public boolean canPass(Entity other) {
