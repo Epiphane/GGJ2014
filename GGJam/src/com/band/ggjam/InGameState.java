530efdeaa0278ed21c7171ab71aee2384cc90a45
Thomas Steinke <exyphnos@gmail.com>
2014-01-26 12:08:35 -0800

    Needs a switch

--- GGJam/src/com/band/ggjam/InGameState.java
+++ GGJam/src/com/band/ggjam/InGameState.java
@@ -19,7 +19,7 @@ public class InGameState extends GameState {
 	private String levelName;
 	
 	public InGameState() {	
-		levelName = "level1";
+		levelName = "level4";
 		
 		camera = new OrthographicCamera(GGJam.GAME_WIDTH, GGJam.GAME_HEIGHT);
 		spriteBatch = new SpriteBatch(100);
