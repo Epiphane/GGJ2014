13464fa7a8468e70e942e2d6902bddcbb160b34b
Blips <mlinsenb@calpoly.edu>
2014-01-26 00:00:31 -0800

    Wacky Waving Inflatable Arm Flailing Tube-man
    Implemented audio playback in the game and edited files for
    gapless(hopefully) playback. Let me know how level switching works so I
    can add in music changes accordingly.

--- GGJam/src/com/band/ggjam/InGameState.java
+++ GGJam/src/com/band/ggjam/InGameState.java
@@ -1,16 +1,41 @@
 package com.band.ggjam;
 
+import java.io.File;
+import java.util.logging.FileHandler;
+
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class InGameState extends GameState {
 	private Level currentLevel;
 	
+	protected Music initMusic;
+	protected Music swapMusic;
+	
 	public InGameState() {		
 		camera = new OrthographicCamera(GGJam.GAME_WIDTH, GGJam.GAME_HEIGHT);
 		spriteBatch = new SpriteBatch(100);
 		
-		currentLevel = new Level("tutorial", this);
+		// Get and set music variables for Tutorial Level
+		FileHandle swapHandler = null;
+		FileHandle initHandler = null;
+		try{
+			swapHandler = Gdx.files.internal("audio/tut.wave.wav");
+			initHandler = Gdx.files.internal("audio/tut.dot.wav");
+		}catch(Exception e) {
+			System.out.println("Encountered error: " + e);
+		}
+		initMusic = Gdx.audio.newMusic(initHandler);
+		swapMusic = Gdx.audio.newMusic(swapHandler);
+		swapMusic.setLooping(true);
+		initMusic.setLooping(true);
+		swapMusic.setVolume(0);
+		initMusic.setVolume(1);
+		currentLevel = new Level("tutorial", this, initMusic, swapMusic);
 	}
 	
 	@Override
@@ -31,7 +56,7 @@ public class InGameState extends GameState {
 	public void beatLevel(String nextLevel) {
 		if(nextLevel != null) {
 			currentLevel.dispose();
-			currentLevel = new Level(nextLevel, this);
+			currentLevel = new Level(nextLevel, this, initMusic, swapMusic);
 		}
 	}
 }
